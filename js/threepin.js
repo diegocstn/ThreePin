53332b3c0791ee3244780e387cf28ca0ce17c7b7
Diego Costantino <diegocstn@gmail.com>
2013-08-10 10:22:33 +0200

    Structure

---
+++ js/threepin.js
@@ -0,0 +1,18 @@
+var ThreePin = (function(){
+	var url,
+		port,
+		connect,
+		status,
+		sectionStatus,
+		STATUS_ENUM	= {
+			CONNECTED		: 1,
+			DISCONNECTED	: 0
+		};
+
+	function init(){
+
+	}
+
+	init();
+
+})();
