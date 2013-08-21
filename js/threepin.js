4c8a73d66b97e25e2183c73080fde62f517d222d
Diego Costantino <diegocstn@gmail.com>
2013-08-21 17:21:40 +0200

    UI status handler

--- js/threepin.js
+++ js/threepin.js
@@ -1,16 +1,82 @@
 var ThreePin = (function(){
-	var url,
+	var $doc	= document,
+		$addressField,
+		$portField,
+		$connectBtn,
+		socket,
+		url,
 		port,
 		connect,
 		status,
-		sectionStatus,
-		STATUS_ENUM	= {
-			CONNECTED		: 1,
+		$sectionStatus,
+		DEBUG	= true,
+		STATUS	= {
+			CONNECTING		: 1,
+			CONNECTED		: 2,
 			DISCONNECTED	: 0
 		};
 
-	function init(){
+	function log(arg){
+		if ( DEBUG ){
+			console.log( arg );
+		}
+	}
 
+	function init(){
+		$addressField	= $doc.querySelector('#conf-url');
+		$portField		= $doc.querySelector('#conf-port');
+		$connectBtn		= $doc.querySelector('#conf button');
+
+		if ( $connectBtn.addEventListener ){
+			$connectBtn.addEventListener( 'click' , initSocket , false );
+		} else if (el.attachEvent){
+			$connectBtn.attachEvent( 'onclick' , initSocket );
+		}
+
+	}
+
+	function initSocket(){
+		var connectionString;
+
+		socketStatusHandler( STATUS.CONNECTING );
+
+		// store address ad port value
+		url		= $addressField.value;
+		port	= $portField.value;
+
+		// build connection string
+		connectionString = url+":"+port;
+
+		// connect and bind events
+		socket = io.connect( connectionString);
+		socket.on('connect', function () {
+			socketStatusHandler( STATUS.CONNECTED );
+		});
+
+		socket.on('disconnect', function () {
+			socketStatusHandler( STATUS.DISCONNECTED );
+		});
+	}
+
+	// TO-DO : usare classList per modificare status ui. funzione o parte o all'interno dell'handler?
+	function socketStatusHandler( status ){
+		switch( status ){
+
+			case STATUS.CONNECTED :
+				log( 'Connection to: '+connectionString );
+
+			break;
+
+			case STATUS.CONNECTING :
+				log( 'Connection to: '+connectionString );
+
+			break;
+
+			case STATUS.DISCONNECTED :
+				log( 'Disconnected from: '+connectionString );
+			break;
+
+		}
 	}
 
 	init();
--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
-var io = require('socket.io').listen(80);
+var io = require('socket.io').listen(5454);
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+	console.log( 'connection' );
+  //socket.emit('news', { hello: 'world' });
 });
